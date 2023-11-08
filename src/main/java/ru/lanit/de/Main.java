3d0304be5da3ceccd1a03217b18751bb4a7fb2b2
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 21:44:56 +0300

    30%

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -5,12 +5,21 @@ public class Main {
     int aInt = 666;
     int bInt = 8888;
     int multiplication = aInt * bInt;
-    int division = aInt / bInt;
+    int division = bInt / aInt;
     int addition = aInt + bInt;
     int subtraction = bInt - aInt;
     int remainder = bInt % aInt;
-
+    System.out.println("Даны числа А и Б");
+    System.out.println("А = 666 и B = 8888");
+    System.out.println("Умножение А на Б = " + multiplication);
+    System.out.println("Деление Б на А = " + division);
+    System.out.println("Сложение А и Б = "+ addition);
+    System.out.println("Вычитаение А из Б = "+ subtraction);
+    System.out.println("");
   }
 }
 
 
+// Логические операторы всегда тип boolean
+// <> <= >= == !=
+// Операторы && ||

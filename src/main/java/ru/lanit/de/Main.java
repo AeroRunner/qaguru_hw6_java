7473316fec6e3bcff071a4ae598f05558434bc05
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 15:58:15 +0300

    or

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -3,8 +3,6 @@ package ru.lanit.de;
 public class Main {
     public static void  main(String[] args){
         System.out.println("Hello world!");
-        //12
-        //13
-        //14
+
     }
 }

c6d76d3484173a925ed6dbb0e6d8dbea1fc411b7
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 18:10:21 +0300

    init commit

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
        int a = 15;
        int b = 16;
-       String c = "Число";
+       String c = "Число такое е то";
        int d = a + b;
         System.out.println(c +" "+ d);
         System.out.println("Resilt");

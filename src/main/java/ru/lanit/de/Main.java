80632b92ec1304c4860addfc5a54be37075b38c3
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 17:43:52 +0300

    java

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -1,8 +1,12 @@
 package ru.lanit.de;
 
 public class Main {
-    public static void  main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+       int a = 15;
+       int b = 16;
+       String c = "Число";
+       int d = a + b;
+        System.out.println(c +" "+ d);
 
     }
 }

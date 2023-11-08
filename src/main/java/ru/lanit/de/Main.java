1ce285c9f9467ea2cfe07c9eef9f705035ca20a7
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 18:26:17 +0300

    razdelenie

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -1,11 +1,10 @@
 package ru.lanit.de;
-
+// master
 public class Main {
     public static void main(String[] args){
        int a = 15;
        int b = 16;
        String c = "Число такое е то";
-       // master
        int d = a + b;
         System.out.println(c +" "+ d);
 

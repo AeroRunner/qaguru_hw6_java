4502a9145cb724704c3388a849c15f3be89d626c
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 22:08:49 +0300

    40%

--- src/main/java/ru/lanit/de/Main.java
+++ src/main/java/ru/lanit/de/Main.java
@@ -2,8 +2,10 @@ package ru.lanit.de;
 // master
 public class Main {
   public static void main(String[] args) {
+
     int aInt = 666;
     int bInt = 8888;
+
     int multiplication = aInt * bInt;
     int division = bInt / aInt;
     int addition = aInt + bInt;
@@ -15,7 +17,29 @@ public class Main {
     System.out.println("Деление Б на А = " + division);
     System.out.println("Сложение А и Б = "+ addition);
     System.out.println("Вычитаение А из Б = "+ subtraction);
-    System.out.println("");
+    System.out.println("Остаток от деления Б на А = " + remainder);
+
+    int cInt = 500;
+    int dInt = 2500;
+    System.out.println("Даны числа C и D");
+    System.out.println("C = 500 и D = 2500");
+    System.out.println("C меньше D? Ответ:"+(cInt < dInt));
+    System.out.println("C больше D? Ответ:"+(cInt > dInt));
+    System.out.println("C равно D? Ответ:" + (cInt==dInt));
+    System.out.println("C не равно D? Ответ:"+(cInt != dInt));
+    System.out.println("C больше или равно D? Ответ:" +(cInt>=dInt));
+    System.out.println("С меньше или равно D? Ответ:"+(cInt<=dInt));
+
+    int income = 100000;
+    int age = 21;
+    int creditRate = 80;
+    if (age>=21){
+      if (income>100000);
+      if (creditRate>=80);
+      System.out.println("Вы подходите под условия кредитования!");
+    }
+
+
   }
 }
 

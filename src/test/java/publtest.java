dc20650adcc254ff803a29354cd13a5c12693650
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-08 13:04:35 +0300

    init commit

---
+++ src/test/java/publtest.java
@@ -0,0 +1,9 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class publtest {
+    @BeforeAll
+    static void asaas(){
+        Configuration.browserSize = "1920x1080";
+    }
+}

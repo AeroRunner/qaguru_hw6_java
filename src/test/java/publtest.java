2d589ba99739d9d0e523dc0c55233061bc7ff1e7
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-09 09:47:57 +0300

    delete test directory

--- src/test/java/publtest.java
+++
@@ -1,9 +0,0 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-
-public class publtest {
-    @BeforeAll
-    static void asaas(){
-        Configuration.browserSize = "1920x1080";
-    }
-}
